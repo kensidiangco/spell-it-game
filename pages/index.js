fb4e008a69e48f930a65fd2f7d604d204d7591cb
kensidiangco <kensidiangco@gmail.com>
2022-10-09 05:32:23 +0800

    fixing missing key prop

--- pages/index.js
+++ pages/index.js
@@ -220,6 +220,7 @@ export default function Home() {
             <AnimatePresence>
               {correctAnswer?.map((answer, i) => (
                 <motion.div 
+                  key={i}
                   initial="hidden" 
                   animate="visible" 
                   variants={{
