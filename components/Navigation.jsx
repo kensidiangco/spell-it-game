3f39eed079b540683482817afc0afae02a9af174
kensidiangco <kensidiangco@gmail.com>
2022-10-27 15:12:39 +0800

    Removing contact us in navigation

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -8,9 +8,9 @@ function Navigation() {
             <span className="p-4 text-5xl text-[#CF6E33] drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] font-bold cursor-pointer rounded-md transition transition-delay-1">Spell It</span>
         </Link>
         <span class="flex gap-4 text-white">
-          <Link href="#" passHref>
+          {/* <Link href="#" passHref>
               <span className="p-2 px-4 text-md border-2 border-green-300 rounded-xl font-bold cursor-pointer transition transition-delay-1 shadow-xl drop-shadow-xl">Contact us</span>
-          </Link>
+          </Link> */}
           <Link href="/About" passHref>
               <span className="p-2 px-4 text-md border-2 border-rose-500 rounded-xl font-bold cursor-pointer transition transition-delay-1 shadow-xl drop-shadow-xl">About us</span>
           </Link>
